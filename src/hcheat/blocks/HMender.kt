1c0de4caf7cec59c045ca423c96b9698a5aac368
Arsen1yGD <a02510735@gmail.com>
2025-05-30 19:15:41 +0300

    add mender + refactor walls sprite

---
+++ src/hcheat/blocks/HMender.kt
@@ -0,0 +1,26 @@
+package hcheat.blocks
+
+import mindustry.type.Category
+import mindustry.type.ItemStack
+import mindustry.world.blocks.defense.MendProjector
+import mindustry.world.meta.BlockGroup
+import mindustry.world.meta.BuildVisibility
+
+class HMender(name: String = "mender") : MendProjector(name) {
+    init {
+        super.init()
+        HBlocks.setupBlock(this)
+        requirements(Category.effect, BuildVisibility.shown, ItemStack.with())
+
+        range = 32F * 30F
+        reload = 1F
+        group = BlockGroup.projectors
+        healPercent = 100F
+    }
+
+    inner class HMenderBuild : MendBuild() {
+        override fun handleDamage(amount: Float): Float {
+            return 0F
+        }
+    }
+}
